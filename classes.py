0a3fd717b659673280334f1d1654e1de520d5434
chloehammond <clhammond821@gmail.com>
2022-04-22 15:17:30 -0500

    Added the docstrings

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
 class Television:
+    '''
+    Class to represent the television objects
+    '''
     MIN_CHANNEL = 0     # Minimum TV channel
     MAX_CHANNEL = 3     # Maximum TV channel
 
@@ -6,12 +9,19 @@ class Television:
     MAX_VOLUME = 2      # Maximum TV volume
 
     def __init__(self):
-        self.__channel = Television.MIN_CHANNEL
-        self.__volume = Television.MIN_VOLUME
-        self.__status = False
+        '''
+        Method to set the default state of the tv
+        '''
+        self.__channel: int = Television.MIN_CHANNEL
+        self.__volume: int = Television.MIN_VOLUME
+        self.__status: bool = False
         pass
 
     def power(self):
+        '''
+        Method to turn the tv on and off
+        :return:
+        '''
         if self.__status is False:
             self.__status = True
         elif self.__status is True:
@@ -19,6 +29,9 @@ class Television:
         pass
 
     def channel_up(self):
+        '''
+        Method to turn the channel up
+        '''
         if self.__status is True:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
@@ -27,6 +40,9 @@ class Television:
         pass
 
     def channel_down(self):
+        '''
+        Method to turn the channel down
+        '''
         if self.__status is True:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -35,6 +51,9 @@ class Television:
         pass
 
     def volume_up(self):
+        '''
+        Method to turn the volume up
+        '''
         if self.__status is True:
             if self.__volume == Television.MAX_VOLUME:
                 self.__volume = Television.MAX_VOLUME
@@ -43,6 +62,9 @@ class Television:
         pass
 
     def volume_down(self):
+        '''
+        Method to turn the volume down
+        '''
         if self.__status is True:
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
@@ -51,6 +73,10 @@ class Television:
         pass
 
     def __str__(self):
+        '''
+        Method to relay the status of the tv
+        :return: The current status of each object of the tv
+        '''
         if self.__status is True:
             return f'TV status: Is on = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
         elif self.__status is False:
