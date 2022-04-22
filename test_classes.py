4ed4b09f533e16e72f85f196b88ce205d804a8e5
chloehammond <clhammond821@gmail.com>
2022-04-22 16:29:21 -0500

    Added volume_up and volume_down tests

--- test_classes.py
+++ test_classes.py
@@ -41,4 +41,27 @@ class Test:
         self.tv.channel_down()
         assert self.tv.__str__() == 'Tv status: Is on = True, Channel = 1, Volume = 0'
 
+    def test_volume_up(self):
+        self.tv.volume_up()
+        assert self.tv.__str__() == 'Tv status: Is on = False, Channel = 0, Volume = 0'
 
+        self.tv.power()
+        self.tv.volume_up()
+        assert self.tv.__str__() == 'Tv status: Is on = True, Channel = 0, Volume = 1'
+
+        self.tv.volume_up()
+        self.tv.volume_up()
+        self.tv.volume_up()
+        assert self.tv.__str__() == 'Tv status: Is on = True, Channel = 0, Volume = 3'
+
+    def test_volume_down(self):
+        self.tv.volume_down()
+        assert self.tv.__str__() == 'Tv status: Is on = False, Channel = 0, Volume = 0'
+
+        self.tv.power()
+        self.tv.volume_down()
+        assert self.tv.__str__() == 'Tv status: Is on = True, Channel = 0, Volume = 0'
+
+        self.tv.volume_down()
+        self.tv.volume_down()
+        assert self.tv.__str__() == 'Tv status: Is on = True, Channel = 0, Volume = 0'
