22d43bc54986f7d0ac90899d7cb36308edbc9848
chloehammond <clhammond821@gmail.com>
2022-04-22 18:14:26 -0500

    Added test in volume_down to get different volume value

--- test_classes.py
+++ test_classes.py
@@ -66,3 +66,8 @@ class Test:
         self.tv.volume_down()
         self.tv.volume_down()
         assert self.tv.__str__() == 'TV status: Is on = True, Channel = 0, Volume = 0'
+
+        self.tv.volume_up()
+        self.tv.volume_up()
+        self.tv.volume_down()
+        assert self.tv.__str__() == 'TV status: Is on = True, Channel = 0, Volume = 1'
